02f7470faaa20812f88884e4884d8adb17e9bf35
metam0rph <abhish7011@gmail.com>
2024-11-27 12:50:29 +0530

    changes for filter bar

--- br_frontend/src/app/components/app-layout/header/header.component.ts
+++ br_frontend/src/app/components/app-layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   opened: boolean = true;
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
+  ngOnInit(): void {
+    // Close the sidenav for mobile devices
+    this.breakpointObserver.observe([Breakpoints.Handset]).subscribe(result => {
+      this.opened = !result.matches; // Close if on mobile
+    });
+  }
   toggleSidenav(): void {
     this.opened = !this.opened;
   }
