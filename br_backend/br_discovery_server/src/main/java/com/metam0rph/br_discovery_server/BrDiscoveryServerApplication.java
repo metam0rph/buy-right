272854d3b11d0b39950c29dbe88a99ef0d1988ba
SkilCode <abhishek1998ashu@gmail.com>
2025-01-12 21:44:58 +0530

    java services added

--- br_backend/br_discovery_server/src/main/java/com/metam0rph/br_discovery_server/BrDiscoveryServerApplication.java
+++ br_backend/br_discovery_server/src/main/java/com/metam0rph/br_discovery_server/BrDiscoveryServerApplication.java
@@ -2,8 +2,10 @@ package com.metam0rph.br_discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BrDiscoveryServerApplication {
 
 	public static void main(String[] args) {
